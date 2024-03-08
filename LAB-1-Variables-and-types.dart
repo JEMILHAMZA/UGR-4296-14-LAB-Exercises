fdfe1d4c98ca0da1e8bf276ba852a0ce5d95747d
jemilshikuri <162720370+jemilshikuri@users.noreply.github.com>
2024-03-08 06:47:46 -0800

    Create LAB-1-Variables-and-types.dart

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "Jemil Shikuri Hamza";
+  int age = 22;
+  String favoriteColor = "Red";
+
+  print("My name is $name, I am $age years old, and my favorite color is $favoriteColor.");
+
+}
+
+
