f1e9986a84573a4381b0fcc4581efa4851269d10
jemilshikuri <162720370+jemilshikuri@users.noreply.github.com>
2024-04-05 16:16:58 +0300

    Update main.dart

--- LAB-2/UI_2/lib/main.dart
+++ LAB-2/UI_2/lib/main.dart
@@ -1,1 +1,218 @@
 
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IJ Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.black),
+                    onPressed: () {
+                      // Implement back button action here
+                    },
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.shopping_cart, color: Colors.black),
+                        onPressed: () {
+                          // Implement shopping cart button action here
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.notifications, color: Colors.black),
+                        onPressed: () {
+                          // Implement notifications button action here
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Card(
+                margin: EdgeInsets.all(0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 200, // Set image height
+                      width: 200, // Set image width
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/lamp.jpg'), // Load image from asset
+                          fit: BoxFit.fill, // Adjust BoxFit
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  '\$10.0',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.favorite_border),
+                                  onPressed: () {
+                                    // Implement icon press action here
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Description line 1',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                Text(
+                                  'Description line 2',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Icon(Icons.star),
+                              Text('5.0'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text('540 Sale'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Icon(Icons.location_on),
+                              Text('AddisAbaba'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              color: Colors.grey[200],
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Size Choices',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        'Color Choices',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Option 1',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      Text(
+                        'Red',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              color: Colors.grey[200],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.save),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement add to shopping cart action here
+                    },
+                    child: Text('Add to Shopping Cart'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
